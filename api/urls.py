3adfd7a9e6ad8687d984f6ef0fa484d95064b5f0
Saran3072 <saran30702@gmail.com>
2023-05-06 00:20:32 +0530

    first commit

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from api import views
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    # path('', views.home, name='index'),
+    path('', TemplateView.as_view(template_name = 'index.html')),
+    path('<slug:id>', views.pred),
+]
---
+++ api/views.py
@@ -0,0 +1,14 @@
+import json
+from pathlib import Path
+import pandas as pd
+import os
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+BASE_DIR = Path(__file__).resolve().parent.parent
+def pred(request, id = None):
+    ans = id
+    path1 = os.path.join(BASE_DIR, 'api/output/output.csv')
+    df = pd.read_csv(path1)
+    dic = df.set_index('IDLink').T.to_dict('list')
+    buff = dic[ans]
+    return JsonResponse({"SentimentTitle": buff[0], "SentimentHeadline": buff[1],})
