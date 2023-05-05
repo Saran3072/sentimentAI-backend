18c81ed7346656b29bc8422a352a7cf235847d0e
Saran3072 <saran30702@gmail.com>
2023-05-06 00:40:13 +0530

    first commit

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     # path('', views.home, name='index'),
-    path('', TemplateView.as_view(template_name = 'index.html')),
+    path('https://sentiback.onrender.com', TemplateView.as_view(template_name = 'index.html')),
     path('<slug:id>', views.pred),
 ]
